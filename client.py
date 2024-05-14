4a4ee0b888a72a1f2b470f7ec28f87fb12267c08
Orazio Spina <orazio.spina@studio.unibo.it>
2024-05-14 11:42:18 +0200

    Added comments

--- client.py
+++ client.py
@@ -2,63 +2,104 @@ import tkinter as tk
 from tkinter import scrolledtext
 import socket
 import threading
+import sys
 
+# Classe che rappresenta il client, estende la classe per creare un app di Tkinter
 class ChatClient(tk.Tk):
+    
+    # Metodo di iniziallizare i nuovi oggetti
     def __init__(self, client_socket):
         super().__init__()
+        
+        # Salvo in una variabile locale la socket associata al client
         self.client_socket = client_socket
+        
         self.title("Chat Client")
         
+        # Gestisco la chiusura della finestra in modo tale da disconnettere il client dal server
         self.protocol("WM_DELETE_WINDOW", self.handle_close)
         
+        # Lista dei messaggi ricevuti
         self.chat_history = scrolledtext.ScrolledText(self, width=60, height=20)
         self.chat_history.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
         
+        # Campo per inserire i messaggi da inviare
         self.entry = tk.Entry(self, width=40)
         self.entry.grid(row=1, column=0, padx=10, pady=10)
         self.entry.bind("<Return>", self.send_message)
         
+        # Pulsante per inviare il messaggio inserito
         self.send_button = tk.Button(self, text="Send", command=self.send_message)
         self.send_button.grid(row=1, column=1, padx=10, pady=10)
         
-        # Start a thread to receive messages
+        # Creo ed avvio un server che gestisce la ricezzione dei nuovi messaggi
         self.receive_thread = threading.Thread(target=self.receive_messages)
         self.receive_thread.start()
 
+    # Metodo per inviare il nuovo messaggio al server
     def send_message(self, event=None):
         message = self.entry.get()
         if message:
-            self.client_socket.send(message.encode())
-            self.entry.delete(0, tk.END)
+            # Se il messaggio non é vuoto mando il messaggio
+            try:
+                self.client_socket.send(message.encode())
+                self.entry.delete(0, tk.END)
+            except Exception:
+                # Gestisco una possibile eccezzione generata dal invio del messaggio
+                print("Errore nel invio del messaggio al server")
 
+    # Metodo per la gestione della chiusura del client
     def handle_close(self):
+        # Chiudo la socket e chiudo la finestra della gui
         self.client_socket.close()
         self.after(500, self.destroy)
 
+    # Metodo per la gestione della ricezione dei messaggi
     def receive_messages(self):
         while True:
             try:
                 message = self.client_socket.recv(1024).decode()
                 if message:
+                    # Se ricevo un messaggio non vuoto allora lo inserisco nella chat
                     self.chat_history.insert(tk.END, f"{message}\n")
                     self.chat_history.see(tk.END)
                 else:
+                    # Se riveco un messaggio di lunghezza zero allora vuol dire che sono stato disconnesso dal server quindi esco dal ciclo (smetto di ricevere messaggi)
                     print("Sarai disconnesso dal server...")
                     break
             except (Exception, OSError):
+                # Gestisco gli errori generati dalla chiusura del client
                 print("Sarai disconnesso dal server...")
                 break
-            
+        
+        # Gestisco la chiusura del clients
         self.handle_close()
 
 if __name__ == "__main__":
+    # Chiedo al utente l'ip e la porta del server
     HOST = input("Insert the ip of the server: ")
     PORT = int(input("Insert the port the server is listening to: "))
-    name = input("Insert your name: ")
-
+    
+    # Chiedo al utente il nickname che vuole utilizzare nella chat, se non inserisce nulla continuo a chiederglielo
+    name = None
+    while (not name):
+        name = input("Insert your name: ")
+        
+    # Creo la socket del client
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((HOST, PORT))
-    client_socket.send(name.encode())
+    
+    # Provo a connettermi con il server
+    try:
+        # Faccio la richiesta di connessione
+        client_socket.connect((HOST, PORT))
+        
+        # Mando il nickname che ho scelto
+        client_socket.send(name.encode())
+    except ConnectionRefusedError:
+        # Gestisco il fatto che il server sia spento oppure l'indirizzo o porta sbagliati
+        print("Server irraggiungibile")
+        sys.exit(0)
 
+    # Creo una nuova istanza della view e la lancio
     app = ChatClient(client_socket)
     app.mainloop()
--- server.py
+++ server.py
@@ -1,31 +1,38 @@
 import socket
 import threading
 
+BUFFER_SIZE = 1024
+
+# Inizializzazione lista contenete tutti i client connessi al server
 clients = []
+
+# Funzione per la gestione della rimozione dei client che si disconnessi, rimuovo il client dalla lista, stampo nel terminale del server che l'utente si é disconnesso e faccio una broadcast a tutti informando che l'utente di é disconnesso
+def client_remove(client_socket, name):
+    clients.remove((client_socket, name))
+    print(f"[DISCONNESSIONE] {name} disconnesso.")
+    broadcast(f"{name} si é disconnesso")
 
 # Funzione per gestire i client
 def handle_client(client_socket, addr, name):
     print(f"[NUOVA CONNESSIONE] {name} connesso.")
     
+    # Loop per controllare la ricezione dei dati da parte del client
     while True:
-        # Ricevi i dati dal client
         try:
-            data = client_socket.recv(1024)
+            data = client_socket.recv(BUFFER_SIZE)
             if data:
+                # Se ho ricevuto qualcosa da parte del client allora stampo il messaggio nel terminale del server e faccio una broadcast a tutti client del messaggio
                 message = (name, data.decode())
                 print(message)
                 broadcast(f"{message[0]}: {message[1]}")
             else:
+                # Se ricevo un messaggio di lunghezza zero vuol dire che il client ha fatto una close sulla socket e quindi chiudo la connessione, lo rimuovo dalla lista dei client connessi
                 client_socket.close()
-                clients.remove((client_socket, name))
-                print(f"[DISCONNESSIONE] {name} disconnesso.")
-                broadcast(f"{name} si é disconnesso")
+                client_remove(client_socket, name)
                 break
         except (Exception, OSError):
-            client_socket.close()
-            clients.remove((client_socket, name))
-            print(f"[DISCONNESSIONE] {name} disconnesso.")
-            broadcast(f"{name} si é disconnesso")
+            # Gestisco le eccezzioni dovute dalla chiusura del client da parte del server
+            client_remove(client_socket, name)
             break
 
 # Funzione per inviare messaggi a tutti i client
@@ -33,11 +40,13 @@ def broadcast(message):
     for client in clients:
         client[0].send(message.encode())
 
+# Funzione per avviare il server
 def serverStart():
-    # Configurazione del server
+    # Configurazione dei dati del server
     HOST = '0.0.0.0'
     PORT = 5555
 
+    # Iniziallizazione socket server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # permette di riutilizzare l'indirizzo
     server.bind((HOST, PORT))
@@ -45,28 +54,40 @@ def serverStart():
 
     print(f"[SERVER] In ascolto su {HOST}:{PORT}")
 
+    # Gestisco le nuove richieste di connessione al server
     try:
         while True:
+            # Accetto la richiesta del client
             client_socket, addr = server.accept()
+            
+            # Aspetto che il client invii il suo nickname
             data = client_socket.recv(1024)
             if data:
                 name = data.decode()
             
+                # Creo ed avvio il thread che gestisce il nuovo client (ascolta se manda dei messaggi)
                 client_thread = threading.Thread(target=handle_client, args=(client_socket, addr, name))
                 client_thread.start()
                 
+                # Comunico agli altri utenti che un nuovo client si é connesso
                 broadcast(f"{name} si é unito alla chat room")
                 
+                # Aggiungo il nuovo client alla lista
                 clients.append((client_socket, name))
 
     except KeyboardInterrupt:
+        # Gestisco la chiusura del server tramite interrupt Ctrl+C
         for client in clients:
             print(f"\nChiudo il clinet {client[1]}")
+            # Chiudo la scocket con il client
             client[0].close()
+        # Libero la lista dei client
         clients.clear()
         print("\n[SERVER] Tutti i client disconnessi.")
         print("\n[SERVER] Server chiuso.")
+        # Chiudo il server
         server.close()
     
 if __name__ == "__main__":
+    # Chiamo la funzione per avviare il server
     serverStart()
